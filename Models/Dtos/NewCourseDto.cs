27a84863c53bf039a65ac07a90aa3bc8dacc749f
Sickle41 <sickle41@gmail.com>
2025-07-05 17:26:59 -0500

    CourseModel

---
+++ Models/Dtos/NewCourseDto.cs
@@ -0,0 +1,11 @@
+
+
+namespace HealthCareDemo.Models.DTOs;
+
+public class NewCourse
+{
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public string Category { get; set; }
+
+}
